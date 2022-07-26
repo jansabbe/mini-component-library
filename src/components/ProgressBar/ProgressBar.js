34401a9caf9fdeb7e7b3c4ad7b0e5ddb971ff897
Jan Sabbe <648689+jansabbe@users.noreply.github.com>
2022-07-26 14:56:31 +0200

    Implement progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,55 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const BaseWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const BarWrapper = styled.div`
+  height: 100%;
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
+const LargeWrapper = styled(BaseWrapper)`
+  border-radius: 8px;
+  height: 24px;
+  padding: 4px;
+`;
+
+const MediumWrapper = styled(BaseWrapper)`
+  border-radius: 4px;
+  height: 12px;
+`;
+
+const SmallWrapper = styled(BaseWrapper)`
+  border-radius: 4px;
+  height: 8px;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const Wrapper = {
+    large: LargeWrapper,
+    medium: MediumWrapper,
+    small: SmallWrapper,
+  }[size];
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value}>
+      <BarWrapper>
+        <Bar style={{ width: `${value}%` }} />
+      </BarWrapper>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
